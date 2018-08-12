962cd4ae150fc585dd88cfc33c5c7d77b1f57bf3
joeyism <sham.joey@gmail.com>
2018-08-11 21:01:06 -0400

    Added tensorboard

--- train.py
+++ train.py
@@ -16,12 +16,21 @@ optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
 correct_pred = tf.equal(tf.argmax(model.out, 1), tf.argmax(y, 1))
 accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 
+tf.summary.histogram("cost", cost)
+tf.summary.histogram("accuracy", accuracy)
+
 cifar = Cifar(batch_size=batch_size)
 
 init = tf.initialize_all_variables()
 
+saver = tf.train.Saver()
+i = 0
 with tf.Session() as sess:
     sess.run(init)
+
+    reader = tf.WholeFileReader()
+    writer = tf.summary.FileWriter( './logs/', sess.graph)
+
     for epoch in range(no_of_epochs):
         for batch in tqdm(cifar.batches,
                           desc="Epoch {}".format(epoch),
@@ -29,10 +38,14 @@ with tf.Session() as sess:
 
             inp, out = helper.transform_to_input_output(batch, dim=model.n_classes)
 
-            sess.run([optimizer],
+            merge = tf.summary.merge_all()
+            summary, _ = sess.run([merge, optimizer],
                         feed_dict={
                             model.input_images: inp,
                             y: out})
+
+            writer.add_summary(summary, i)
+            i = i + 1
 
         acc = sess.run(accuracy,
                        feed_dict={
